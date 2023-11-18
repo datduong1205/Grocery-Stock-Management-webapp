c524f0c56c1cbedb86a8d28ae280c4cac5ce9d19
Dat Duong <soundgreat12@gmail.com>
2023-11-17 22:34:35 -0500

    add header and footer

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET / page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Home'  
+  });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home'  
+  });
 });
 
 module.exports = router;
