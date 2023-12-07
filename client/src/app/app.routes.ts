f6fabe144521665a0517427f6da112222e964026
Quincy Chan <yanhoquincy.chan@ontariotechu.net>
2023-12-07 17:58:18 -0500

    Angular Addition

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { GroceryComponent } from './pages/grocery/grocery.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LogoutComponent } from './pages/logout/logout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'about', component: AboutComponent},
+    {path:'contactus', component: ContactusComponent},
+    {path:'grocery', component: GroceryComponent},
+    {path:'login', component: LoginComponent},
+    {path:'logout', component: LogoutComponent},
+    {path:'', redirectTo:'/home', pathMatch:'full'}
+];
